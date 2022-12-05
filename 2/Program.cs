c11ee6443bb12576951f4f5abfc1993b9a8c14df
IgorMalakhov <forvord_next@mail.ru>
2022-12-05 08:12:53 +0300

    Home work 2: 1st task complete

---
+++ 2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int SecNum(int num)
+// {
+//     int result;
+//     if(num > 99 && num < 1000)
+//     {
+//         int semiresult = num % 100;
+//         result = semiresult / 10;
+//         return result;
+//     }
+//     else
+//     {
+//         result = -1;
+//         return result;
+//     }
+// }
+
+// Console.Write("Input three-digit number: ");
+// int UserNumber = Convert.ToInt32(Console.ReadLine());
+
+// int NewNum = SecNum(UserNumber);
+// Console.WriteLine("Sec number is " + NewNum);
+
+//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+
+//Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
